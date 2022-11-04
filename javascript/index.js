01f1b564462970843c32948d77d21655cce2a825
LauCarrillo <laucarrillo21@gmail.com>
2022-11-04 12:51:16 -0600

    update JS single location time

---
+++ javascript/index.js
@@ -0,0 +1,21 @@
+// create a function that covers all the cities
+function updateTime() {
+//Los Angeles
+// Select the city
+let losAngelesElement = document.querySelector("#los-angeles");
+//Select the date
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+//Select the time
+let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+//Select Time Zone Moment
+let losAngelesTime = moment().tz("America/Los_Angeles");
+//Modify date using innerHTML
+losAngelesDateElement.innerHTML = losAngelesTime.format("dddd, MMMM Do YYYY");
+//Modify time using innerHTML
+losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>] A [</small>]");
+}
+
+// Call the function
+updateTime();
+//Update everysecond
+setInterval(updateTime, 1000);
